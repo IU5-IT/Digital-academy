0b6bdace4d445ad0cba2be142923f2c5d1b2dad0
Dmitiy <dimapermyakov55@gmail.com>
2022-10-10 14:06:07 +0300

    

---
+++ Week-02/main.py
@@ -0,0 +1,10 @@
+# Copyright 2022 Dmitriy <dimapermyakov55@gmail.com>
+import pygame
+
+
+def main():
+    print('Hi! Welcome to week-2 project. The code will be shown up here soon.')
+
+
+if __name__ == '__main__':
+    main()
