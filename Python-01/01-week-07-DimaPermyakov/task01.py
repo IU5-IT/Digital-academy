da27280ace0653b6702471862934fe98f7692d44
Dmitiy <dimapermyakov55@gmail.com>
2022-11-24 11:57:59 +0300

    Added the first task

--- Python-01/01-week-07-DimaPermyakov/task01.py
+++ Python-01/01-week-07-DimaPermyakov/task01.py
@@ -1,8 +1,24 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
+import time
 
-def main():
-    pass
+
+def time_decorator(func):
+    def wrapper():
+        st = time.time()
+        res = func()
+        print(int(time.time() - st))
+        return res
+
+    return wrapper
+
+
+@time_decorator
+def sleep_1_sec():
+    time.sleep(1)
+    print("function")
+    return 25
 
 
 if __name__ == '__main__':
-    main()
+    result = sleep_1_sec()
+    print(result)
