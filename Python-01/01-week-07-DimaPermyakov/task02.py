2a8268255fecd24b4704fd7071c00cc7db4b5d0e
Dmitiy <dimapermyakov55@gmail.com>
2022-11-24 17:34:38 +0300

    The 3rd task was successfully solved

--- Python-01/01-week-07-DimaPermyakov/task02.py
+++ Python-01/01-week-07-DimaPermyakov/task02.py
@@ -1,8 +1,36 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
+import datetime
+from functools import wraps
+from inspect import getcallargs
 
-def main():
-    pass
+
+def logging_decorator(lst: list):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_answer = datetime.datetime.now()
+            res = func(*args, **kwargs)
+            lst.append({
+                'name': func.__name__,
+                'arguments': getcallargs(func, *args, **kwargs),
+                'call_time': time_answer,
+                'result': res
+            })
+            return res
+
+        return wrapper
+
+    return decorator
+
+
+logger = []
+
+
+@logging_decorator(logger)
+def test_simple(a, b=2):
+    return 127
 
 
 if __name__ == '__main__':
-    main()
+    test_simple(1, 2)
+    print(logger)
