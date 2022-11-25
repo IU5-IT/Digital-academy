e7670fcc330c00d05915cf76eec924376761b537
Dmitiy <dimapermyakov55@gmail.com>
2022-11-25 18:25:34 +0300

    The task 3 was done:

--- Python-01/01-week-07-DimaPermyakov/README.md
+++ Python-01/01-week-07-DimaPermyakov/README.md
@@ -1,7 +1,9 @@
 # Week 7.
+
 <img src="https://img.shields.io/github/license/DimaPermyakov/IU5?color=brightgreen" alt="MIT License"> <img src="https://img.shields.io/badge/language-Python-blue.svg" alt="Python Language">
 
 # [Tasks conditions](https://github.com/IU5-IT/Digital-academy/wiki/Week-7)
+
 - [X] [Task 1](https://github.com/IU5-IT/Digital-academy/blob/main/Python-01/01-week-07-DimaPermyakov/task01.py)
 - [X] [Task 2](https://github.com/IU5-IT/Digital-academy/blob/main/Python-01/01-week-07-DimaPermyakov/task02.py)
-- [ ] [Task 3]()
+- [X] [Task 3](https://github.com/IU5-IT/Digital-academy/blob/main/Python-01/01-week-07-DimaPermyakov/task03.py)
--- Python-01/01-week-07-DimaPermyakov/task03.py
+++ Python-01/01-week-07-DimaPermyakov/task03.py
@@ -1,25 +1,7 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
+import json
 import requests
-
-'''
-"statistics": [
-    {
-      "id": 1,
-      "username": "lolkek",
-      "email": "user1@mail.dot",
-      "posts": 125,
-      "comments": 1358
-    },
-    {
-      "id": 2,
-      "username": "cheburek",
-      "email": "user2@mail.dot",
-      "posts": 5,
-      "comments": 12
-    }
-  ]
-}
-'''
+import pickle
 
 
 def main():
@@ -27,19 +9,20 @@ def main():
     URL_POST = 'https://jsonplaceholder.typicode.com/posts'
     URL_COMMENT = 'https://jsonplaceholder.typicode.com/comments'
     users = requests.get(URL_USER).json()
-    # response = requests.post(URL_BASE, data={"some_param": 124})
     posts = requests.get(URL_POST).json()
     comment = requests.get(URL_COMMENT).json()
-    '''
-    
-    '''
-    res = [{'id': user_id[0],
-            'username': user_id[1],
-            'email': user_id[2],
-            'posts': sum([1 for user_from_post_id in posts if user_id[0] == user_from_post_id['userId']]),
-            'comments': sum([1 for user_from_post_id in comment if user_id[2] == user_from_post_id['email']])} for
-           user_id in [(el['id'], el['username'], el['email']) for el in users]]
-    print(*res, sep='\n')
+    user_info = {'statistics': [
+        {'id': user_id[0],
+         'username': user_id[1],
+         'email': user_id[2],
+         'posts': sum([1 for user_from_post_id in posts if user_id[0] == user_from_post_id['userId']]),
+         'comments': sum([1 for user_from_post_id in comment if user_id[2] == user_from_post_id['email']])}
+        for user_id in [(el['id'], el['username'], el['email']) for el in users]]}
+    URL_BASE = 'https://webhook.site/e13f15bf-ea71-4c97-973d-1d726952e333'
+    response = requests.post(URL_BASE, data=json.dumps(user_info))
+
+    with open("solution.pickle", 'wb') as f:
+        pickle.dump(response, f)
 
 
 if __name__ == '__main__':
